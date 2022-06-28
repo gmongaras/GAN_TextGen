Fixed inplace gradient issues

src/blocks/outTrans.py
@@ -30,7 +30,7 @@ class outTrans(nn.Module):
         
         # Feed-foward block after the MHA blocks
         self.FF = nn.Linear(E_2, FF_embedding, device=device)
-        self.ReLU = nn.ReLU()
+        self.ReLU = nn.ReLU(inplace=False)
         
         # Layer normalization blocks
         self.LN1 = nn.LayerNorm(E_2, device=device)