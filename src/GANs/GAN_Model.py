3f8f9224fbf30fadd036bd55e401f5c155bba289
gmongaras <gabriel@mongaras.com>
2022-08-01 16:23:24 -0700

    Removed redundant print statement

--- src/GANs/GAN_Model.py
+++ src/GANs/GAN_Model.py
@@ -345,7 +345,6 @@ class GAN_Model(nn.Module):
             # Beta changing for n_G
             if epoch % self.Beta_n == 0:
                 self.Beta = self.Beta + np.tanh((np.abs(self.discLoss[-1])-np.abs(self.discLoss[-self.Beta_n]))/self.Beta_n)
-                print(self.Beta)
             
             print(f"Epoch: {epoch}   Generator Loss: {round(genLoss.item(), 4)}     Discriminator Real: {-round(discLoss_real.item(), 4)}     Discriminator Fake: {round(discLoss_fake.item(), 4)}    Discriminator Loss: {round(discLoss.item(), 4)}", end="")
             if self.dynamic_n_G == True:
