Fixed MHA and added masking

src/GANs/GAN_Model.py
@@ -105,6 +105,7 @@ class GAN_Model(nn.Module):
         self.delWhenLoaded = delWhenLoaded if self.loadInEpoch == False else False
         self.embedding_size_disc = embedding_size_disc
         self.costSlope = costSlope
+        self.num_heads = num_heads
         
         # Saving paramters
         self.saveSteps = saveSteps
@@ -164,7 +165,8 @@ class GAN_Model(nn.Module):
     # Inputs:
     #   x - A sample of real data
     #   x_tilde - A sample of data from the generator
-    def get_gradient_penalty(self, x, lens_real, x_tilde, lens_fake):
+    def get_gradient_penalty(self, x, lens_real, x_tilde, lens_fake, \
+        masks_real, masks_fake):
         # Get the device
         device = self.device
         if self.dev == "partgpu":
@@ -178,6 +180,9 @@ class GAN_Model(nn.Module):
         epsilon = epsilon.expand(x.shape)
         x_hat = epsilon*x + (1-epsilon)*x_tilde
 
+        # Masks are the combination of the two masks
+        masks_hat = torch.logical_or(masks_real, masks_fake).clone().detach()
+
         # We only want the gradients of the discriminator
         x_hat = x_hat.clone().detach()
         x_hat = torch.autograd.Variable(x_hat, requires_grad=True)
@@ -186,7 +191,7 @@ class GAN_Model(nn.Module):
         
         # Send the transformed and combined data through the
         # discriminator
-        disc_hat = self.discriminator(x_hat, lens_hat)
+        disc_hat = self.discriminator(x_hat, lens_hat, masks_hat)
         
         # Get the gradients of the discriminator output
         gradients = torch.autograd.grad(outputs=(disc_hat), inputs=(x_hat, lens_hat),
@@ -224,6 +229,70 @@ class GAN_Model(nn.Module):
             
         # Return the output and the masks
         return end_pos.to(self.device if self.dev != "partgpu" else gpu)
+    
+
+    # Given lengths for a sentence, return the MHA mask
+    # for that sentence
+    # Input:
+    #   Tensor of shape (N, S) where each item is the softmax
+    #   encoded length for that sentence
+    # Output:
+    #   Tensor of shape (num_heads*N, L, L) where teh second L stores
+    #   the positions in the sentence to ignore
+    def getMask(self, lens_soft):
+        # Get the argmax of the lengths
+        lens = torch.argmax(lens_soft, dim=-1)
+
+        # # Masks
+        # masks = torch.zeros((self.batchSize, self.sequence_length), dtype=torch.bool)
+
+        # # For each length, create a binary tensor with True
+        # # after the length and False before
+        # for i in range(0, self.batchSize):
+        #     l = lens[i]
+        #     m = masks[i]
+
+        #     m[l+1:] = True
+        #     masks[i] = m
+
+
+        """
+        What do the masks look like?
+        Sequence: 'word word word <END> <PAD> <PAD>'
+        Size: (1, 6, E)
+        Mask:
+        [[0, 0, 0, 0, -inf, -inf]
+         [0, 0, 0, 0, -inf, -inf]
+         [0, 0, 0, 0, -inf, -inf]
+         [0, 0, 0, 0, -inf, -inf]
+         [0, 0, 0, 0, -inf, -inf]
+         [0, 0, 0, 0, -inf, -inf]]
+
+        So the mask is of shape (N, L, L) where the mask has
+        -inf after the length and 0 before the length in the Nth
+        sequence
+
+        Why are the -infs in the columns and not the rows?
+        When the matrix S = soft(QK) is created, the columns will be multiplied
+        by the rows in the V matrix, as matrix multiplication goes. So, the columns
+        with -inf will be multiplied by the row with the same index, which will be
+        the row with the <PAD> encoding in it.
+        """
+
+
+        # Create a 2-d mask of each sequene
+        masks = torch.zeros((self.batchSize, self.sequence_length, self.sequence_length), dtype=torch.float)
+
+        # For each length, create a binary tensor with True
+        # after the length and False before
+        for i in range(0, self.batchSize):
+            l = lens[i]
+            masks[i, :, min(l+1, self.sequence_length):] = -np.inf
+
+        # Broadcast the masks over the number of heads
+        masks = torch.repeat_interleave(masks, self.num_heads, dim=0)
+
+        return masks.to(self.device if self.dev != "partgpu" else gpu)
     
     
     
@@ -268,7 +337,7 @@ class GAN_Model(nn.Module):
             # Train the discriminator first or if dynamic_n is used and
             # r_d > r_g
             self.optim_disc.zero_grad()
-            for i in range(0, max(self.n_D, 1) if self.dynamic_n == False else (1 if r_d > self.Lambda_n*r_g or L_p_g == None else 0)):
+            for i in range(0, max(self.n_D, 1) if self.dynamic_n == False else 1):
                 # Sample data for the discriminator
                 disc_sub = disc_nums[:self.batchSize]
                 disc_nums = disc_nums[self.batchSize:]
@@ -284,10 +353,13 @@ class GAN_Model(nn.Module):
                 # Generate some data from the generator
                 with torch.no_grad():
                     fake_X, lens_fake = self.generator.forward_(training=False)
+
+                # Get the masks for the generator
+                masks_fake = self.getMask(lens_fake)
                 
                 # Send the generated output through the discriminator
                 # to get a batch of predictions on the fake sentences
-                disc_fake = self.discriminator(fake_X, lens_fake, masks)
+                disc_fake = self.discriminator(fake_X, lens_fake, masks_fake)
                 
                 # Get a real data subset using one_hot encoding
                 if self.loadInEpoch == True:
@@ -322,10 +394,10 @@ class GAN_Model(nn.Module):
                 lens_real = self.getLens(real_X)
                 
                 # Get the variance
-                sampleSize = 100000
-                maxVariance = len(self.vocab)/100
-                minVariance = 0.1
-                variance = torch.linspace(maxVariance, minVariance, epochs)[epoch]
+                # sampleSize = 100000
+                # maxVariance = len(self.vocab)/100
+                # minVariance = 0.1
+                # variance = torch.linspace(maxVariance, minVariance, epochs)[epoch]
                 
                 # # Iterate over all non-<PAD> tokens in the real data
                 # # and spread the data distribution
@@ -357,8 +429,9 @@ class GAN_Model(nn.Module):
                 #         # replace the one-hot vector
                 #         real_X[i, j] = N_disc.to(self.device)
 
-                # One hot encode the lengths
+                # One hot encode the lengths and masks
                 lens_real = torch.nn.functional.one_hot(lens_real, self.sequence_length).float()
+                masks_real = self.getMask(lens_real)
 
                 # Make the inputs into the discriminator loss variables
                 # for differentiation
@@ -368,14 +441,15 @@ class GAN_Model(nn.Module):
                 lens_fake = torch.autograd.Variable(lens_fake, requires_grad=True)
 
                 # Get the gradient penalty
-                gradient_penalty = self.get_gradient_penalty(real_X, lens_real, fake_X, lens_fake)
+                gradient_penalty = self.get_gradient_penalty(real_X, lens_real, fake_X, lens_fake, \
+                    masks_real, masks_fake)
 
                 # Calculate the mean reconstruction error for debugging
                 MRE = torch.abs(real_X-fake_X).sum(-1).sum(-1).mean()
                 
                 # Send the generated output through the discriminator
                 # to get a batch of predictions on the real sentences
-                disc_real = self.discriminator(real_X, lens_real, masks)
+                disc_real = self.discriminator(real_X, lens_real, masks_real)
                 
                 # Get the discriminator loss
                 #discLoss = GLS_disc(disc_real, disc_fake, real_X, fake_X, self.costSlope, "l1")
@@ -387,14 +461,16 @@ class GAN_Model(nn.Module):
                 # The cost of the discriminator is the loss + the penalty
                 discCost = discLoss + gradient_penalty
                 
-                # Backpropogate the cost
-                discCost.backward()
-                
-                # Step the optimizer
-                self.optim_disc.step()
+                if self.dynamic_n == False or (self.dynamic_n == True and r_d > self.Lambda_n*r_g):
+                    # Backpropogate the cost
+                    discCost.backward()
+                    
+                    # Step the optimizer
+                    self.optim_disc.step()
                 self.optim_disc.zero_grad()
 
                 # Delete all discriminator stuff as its no longer needed
+                discCost = discCost.detach()
                 gradient_penalty = gradient_penalty.cpu().detach().item()
                 del disc_sub, real_X, lens_real, lens_fake
             
@@ -412,7 +488,7 @@ class GAN_Model(nn.Module):
 
             # Train the generator next
             self.optim_gen.zero_grad()
-            for i in range(0, 1 if self.dynamic_n == False else (1 if r_d <= self.Lambda_n*r_g or L_p_g == None else 0)):
+            for i in range(0, 1 if self.dynamic_n == False else 1):
                 # Sample real data for the second loss function
                 disc_sub = disc_nums[:self.batchSize]
                 disc_nums = disc_nums[self.batchSize:]
@@ -428,22 +504,26 @@ class GAN_Model(nn.Module):
                 
                 # Generate some data from the generator
                 fake_X, lens_fake = self.generator.forward_(training=True)
+
+                # Get the masks for the generator
+                masks_fake = self.getMask(lens_fake)
                 
                 # Send the generated output through the discriminator
                 # to get a batch of predictions on the fake sentences
-                disc_fake = self.discriminator(fake_X, lens_fake, masks)
+                disc_fake = self.discriminator(fake_X, lens_fake, masks_fake)
                 
                 # Get the generator loss
                 #genLoss = GLS_gen(disc_fake)
                 genLoss = wasserstein_gen(disc_fake)
 
-                
-                # Backpropogate the loss
-                genLoss.backward()
-                
-                # Step the optimizer
-                self.optim_gen.step()
+                if self.dynamic_n == False or (self.dynamic_n == True and r_d <= self.Lambda_n*r_g):
+                    # Backpropogate the loss
+                    genLoss.backward()
+                    
+                    # Step the optimizer
+                    self.optim_gen.step()
                 self.optim_gen.zero_grad()
+                genLoss = genLoss.detach()
             
                 # Delete all generator stuff as its no longer needed
                 del disc_sub, fake_X, lens_fake
@@ -460,7 +540,8 @@ class GAN_Model(nn.Module):
             
             
             # Flip the maximizing values to represent the actual value
-            discLoss_fake *= -1
+            discLoss_real *= -1
+            genLoss *= -1
             
             
             # Save the loss values

src/GANs/blocks/MHA.py
@@ -18,9 +18,9 @@ class MHA(nn.Module):
         
         
         # Key, query, value weights
-        self.value_weights = nn.Linear(E_2, output_embedding)
-        self.key_weights = nn.Linear(E_1, output_embedding)
-        self.query_weights = nn.Linear(E_1, output_embedding)
+        self.value_weights = nn.Linear(E_2, output_embedding, bias=False)
+        self.key_weights = nn.Linear(E_1, output_embedding, bias=False)
+        self.query_weights = nn.Linear(E_1, output_embedding, bias=False)
         
         # MHA module
         self.MultiHeadAtt = nn.MultiheadAttention(output_embedding, num_heads, batch_first=True)
@@ -37,9 +37,13 @@ class MHA(nn.Module):
     #   A 3-D tensor of shape (N, S, output_embedding)
     def forward(self, X_1, X_2, masks=None):
         # Get the key, query, value embedings
-        value = self.value_weights(X_2)
-        query = self.query_weights(X_1)
-        key = self.key_weights(X_1)
+        # value = self.value_weights(X_2)
+        # query = self.query_weights(X_1)
+        # key = self.key_weights(X_1)
+
+        value = X_2
+        query = X_1
+        key = X_1
         
         # Get the MHA value and return it
-        return self.MultiHeadAtt(query, key, value, key_padding_mask=masks, need_weights=False)[0]
+        return self.MultiHeadAtt(query, key, value, attn_mask=masks, need_weights=False)[0]

src/GANs/blocks/inTrans.py
@@ -38,19 +38,20 @@ class inTrans(nn.Module):
     def forward(self, X, masks=None):
         X_saved = X.clone()
         X = self.MHA(X, X, masks)
-        X += X_saved
         X = self.LN1(X)
+        X += X_saved
 
         # No residual if the output is different from the input
         if self.E_I != self.E_O:
             X = self.FF1(X)
             X = self.Act(X) + 0
             X = self.FF2(X)
+            X = self.LN2(X)
         else:
             X_saved = X.clone()
             X = self.FF1(X)
             X = self.Act(X) + 0
             X = self.FF2(X)
+            X = self.LN2(X)
             X += X_saved
-        X = self.LN2(X)
         return X

src/GANs/blocks/outTrans.py
@@ -52,21 +52,21 @@ class outTrans(nn.Module):
     #     the output sentence word embeddings
     def forward(self, X_1, X_2):
         X = self.MHA1(X_2, X_2)
-        X += X_2
         X = self.LN1(X)
+        X += X_2
         
         # Add noise using the given noise distribution
         X += self.dist.sample(X.shape).float().to(self.device)
         
         X_saved = X.clone()
         X = self.MHA2(X_1, X)
-        X += X_saved
         X = self.LN2(X)
+        X += X_saved
         
         X_saved = X.clone()
         X = self.FF1(X)
         X = self.Act(X) + 0
         X = self.FF2(X)
-        X += X_saved
         X = self.LN3(X)
+        X += X_saved
         return X

src/GANs/models/Discriminator.py
@@ -35,11 +35,12 @@ class Discriminator(nn.Module):
         self.embed_mode = embed_mode.lower()
         self.embedding_size = embedding_size
 
-        # self.LL1 = nn.Linear(vocab_size, int(math.sqrt(vocab_size)), device=device)
-        # self.LL2 = nn.Linear(int(math.sqrt(vocab_size)), embedding_size, device=device)
-        self.LL1 = nn.Linear(vocab_size, vocab_size//100, device=device)
-        self.LL2 = nn.Linear(vocab_size//100, vocab_size//1000, device=device)
-        self.LL3 = nn.Linear(vocab_size//1000, embedding_size, device=device)
+
+        # Discriminator lookup table (linear without a bias)
+        self.Hot2Enc = nn.Linear(vocab_size, embedding_size, bias=False, device=device)
+        # self.LL1 = nn.Linear(vocab_size, vocab_size//100, device=device)
+        # self.LL2 = nn.Linear(vocab_size//100, vocab_size//1000, device=device)
+        # self.LL3 = nn.Linear(vocab_size//1000, embedding_size, device=device)
         
         # If the embed mode is PCA, use the PCA algorithm to transform
         # the input of shape [vocab size] to the shape [embedding_size]
@@ -98,9 +99,22 @@ class Discriminator(nn.Module):
             pass
             #X = self.encodingTransform(X)
 
-        X = self.LL1(X)
-        X = self.LL2(X)
-        X = self.LL3(X)
+        # Encode the words
+        X = self.Hot2Enc(X)
+
+        # X = self.LL1(X)
+        # X = self.LL2(X)
+        # X = self.LL3(X)
+
+        # # Get the lengths as numerical values
+        # lens_num = torch.argmax(lens, dim=-1).cpu().detach()
+
+        # # Replace the <PAD> tokens with 0 so that the
+        # # discriminator doesn't worry about encoding it
+        # for i in range(0, X.shape[0]):
+        #     x = X[i]
+        #     x[lens_num[i].item()+1:] = 0
+        #     X[i] = x
 
         # # Encode the lengths from shape S to E
         # lens = self.lensEnc(lens)
@@ -113,7 +127,7 @@ class Discriminator(nn.Module):
         
         # Send the inputs through the backbone
         if masks != None:
-            X = self.disc_backbone[0](X, masks)
+            X = self.disc_backbone[0](X, masks) # Only mask the first values
             for b in self.disc_backbone[1:]:
                 X = b(X)
         else:

src/GANs/models/Generator.py
@@ -288,7 +288,7 @@ class Generator(nn.Module):
 
         # Get the length estimation from the second model
         # (N, S, E) -> (N, S, E)
-        lens = lens_sent.clone().detach()
+        lens = lens_sent
         for block in self.lenGen:
             lens = block(lens)
 
@@ -298,11 +298,11 @@ class Generator(nn.Module):
         # (N, S) -> (N, S)
         lens = self.lensDec_S(lens)
 
-        # For each length, replace the values with PAD tokens
-        pad_tok = torch.nn.functional.one_hot(torch.tensor(self.vocab_inv["<PAD>"], dtype=torch.int64, device=self.device, requires_grad=False), len(self.vocab))
-        pad_tok = pad_tok.float().to(self.device)
+        # For each length, replace the value after the estimated
+        # length with 0s. So no gradient and the value will not contribute to
+        # the linear layer in the discriminator
         for i in range(0, lens.shape[0]):
-            out_sent[i, torch.argmax(lens, dim=-1)[i].item()+1:] = pad_tok.clone()
+            out_sent[i, torch.argmax(lens, dim=-1)[i].item()+1:] *= 0
 
         
         # Return the output:

src/helpers/helpers.py
@@ -246,9 +246,12 @@ def addPadding(X, vocab_inv, sequence_length, encoder):
 #   vocab_inv - Inverted vocab where words map to their values
 #   sequence_length - Length to encode each sequence to
 def addPadding_one_hot(X, vocab_inv, sequence_length):
-    # The padding tensor
-    #pad_enc = encoder(torch.tensor(vocab_inv["<PAD>"]))
-    pad_enc = torch.nn.functional.one_hot(torch.tensor(vocab_inv["<PAD>"]), len(vocab_inv))
+    # The padding tensor. Note <END> is used as a temp variable
+    # since <PAD> is not in the vocab anymore
+    pad_enc = torch.nn.functional.one_hot(torch.tensor(vocab_inv["<END>"]), len(vocab_inv))
+
+    # Instead of padding, use 0s
+    pad_enc = pad_enc*0
     
     # The new padded tensor
     X_padded = torch.zeros(len(X), sequence_length, pad_enc.shape[-1])